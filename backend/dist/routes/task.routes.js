644b7707d1787a231cc09074b60dbb92543dd15d
suryaKumar2408 <suryashukla3284@gmail.com>
2026-06-01 21:53:56 +0530

    add .gitignore

--- backend/dist/routes/task.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const task_controller_1 = require("../controllers/task.controller");
-const router = (0, express_1.Router)();
-const taskController = new task_controller_1.TaskController();
-router.patch('/:taskId/status', taskController.updateStatus);
-exports.default = router;
